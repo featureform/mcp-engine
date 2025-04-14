79a30493375c7b9340461b53b5973f206ca8da2d
ff-kamal <kamal@featureform.com>
2025-04-14 08:28:29 -0700

    Combine tool authn/authz tests

--- tests/server/auth/test_errors.py
+++ tests/server/auth/test_errors.py
@@ -9,27 +9,24 @@ from mcpengine.types import AUTHENTICATION_ERROR, AUTHORIZATION_ERROR
 
 
 @pytest.mark.anyio
-async def test_authentication_error():
+async def test_tool_errors():
     mcp = MCPEngine()
+
+    def authz_error():
+        raise AuthorizationError
 
     def authn_error():
         raise AuthenticationError
 
     mcp.add_tool(authn_error)
+    mcp.add_tool(authz_error)
+
     async with client_session(mcp._mcp_server, raise_exceptions=False) as client:
         with pytest.raises(McpError) as errinfo:
             await client.call_tool("authn_error")
         base_error = errinfo.value.error
         assert base_error.code == AUTHENTICATION_ERROR
 
-@pytest.mark.anyio
-async def test_authorization_error():
-    mcp = MCPEngine()
-
-    def authz_error():
-        raise AuthorizationError
-
-    mcp.add_tool(authz_error)
     async with client_session(mcp._mcp_server, raise_exceptions=False) as client:
         with pytest.raises(McpError) as errinfo:
             await client.call_tool("authz_error")
